8fd4cf7f8533fd8f552f3acc8889de66cb3efc0d
enriquesagredo <enrique.sagredo@protonmail.com>
2023-08-29 22:09:49 +0200

    feat. adding new app

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const hbs = require("hbs");
+
+require("./config/db.config");
+
+require("./config/hbs.config");
+
+// Views engine
+app.set("view engine", "hbs");
+app.set("views", `${__dirname}/views`);
+
+// Routes
+const router = require("./config/routes.config");
+app.use(router);
+
+app.listen(3000, () => console.info("Ready"));
