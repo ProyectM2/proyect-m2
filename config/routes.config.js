dbbf5fdf9faada0aa284850a857927ca93ff94be
enriquesagredo <enrique.sagredo@protonmail.com>
2023-09-07 18:39:56 +0200

    feat. config modified

--- config/routes.config.js
+++ config/routes.config.js
@@ -2,16 +2,20 @@ const express = require("express");
 const router = express.Router();
 const home = require("../controllers/home.controller")
 const user = require("../controllers/user.controller")
-
+const secure = require('../middlewares/secure.mid.js');
+const reserve = require("../controllers/travel.controller")
 
 router.get("/", (req, res) => res.redirect("/home"));
 
 router.get("/home", home.home)
 
 router.get("/login", user.login)
+router.post('/login', user.doLogin);
+// router.get('/profile', secure.isAuthenticated, user.profile);
 
 router.get("/register", user.register)
 router.post("/register", user.doRegister)
 
+router.get("/reserve", reserve.reserve)
 
 module.exports = router
--- config/session.config.js
+++ config/session.config.js
@@ -1,8 +1,10 @@
-const session = require("express-session");
+const esession = require("express-session");
 const mongoose = require("mongoose");
 const MongoStore = require("connect-mongo");
+const user = require("../models/user.models")
 
-module.exports = session({
+
+module.exports.session = esession({
   resave: true,
   secret: "super secret",
   saveUninitialized: false,
@@ -10,4 +12,24 @@ module.exports = session({
     mongoUrl: mongoose.connection._connectionString,
     ttl: 60 * 60 * 24 * 7, // 1 semana
   }),
+  cookie: {
+    httpOnly: true
+  }
 });
+
+module.exports.Loadsessionuser = (req, res, next) => {
+
+  const userId = req.session.userId;
+  if (userId) {
+    user.findById(userId)
+    .then((user) => {
+      req.user = user;
+      res.locals.currentUser = user;
+      next();
+    })
+    .catch((error) => next(error));
+  } else {
+    next();
+  }
+
+}
