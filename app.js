3d1fcd4f24378d6114ee545fd25e8a2d89197aa7
enriquesagredo <enrique.sagredo@protonmail.com>
2023-09-10 21:20:25 +0200

    feat.  main css 404 solved

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const morgan = require("morgan");
 const hbs = require("hbs");
 
+app.use('/styles', express.static('styles'));
+
 require("./config/db.config");
 
 require("./config/hbs.config");
