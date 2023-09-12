221e0befb1c7370c0f6d29f199a8fc969a425289
Andrews <andreeews2k12@gmail.com>
2023-09-12 19:21:15 +0200

    feat: homes list

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Travel = require('../models/travel.models')
 
 module.exports.home = (req, res, next) => {
-    Travel.find({ "users": { $size: { $lt: 15 } } })
+    Travel.find()
         .then((travels) => res.render('home', { travels }))
         .catch((error) => next(error))
 }
