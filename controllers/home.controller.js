b24f37da01ead306a73d759bebd02d8e5d0ec07b
Andrews <andreeews2k12@gmail.com>
2023-09-12 19:38:03 +0200

    feat: travel registration list

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Travel = require('../models/travel.models')
 
 module.exports.home = (req, res, next) => {
-    Travel.find()
+    Travel.find({ $expr: { $lt: [{ $size: "$users" }, 15]} })
         .then((travels) => res.render('home', { travels }))
         .catch((error) => next(error))
 }
