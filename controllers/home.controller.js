7bd77d6fb9f5100812a20053c89922b08370dc47
Andrews <andreeews2k12@gmail.com>
2023-09-12 19:50:14 +0200

    fix: date fixed in travel list

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -2,7 +2,20 @@ const mongoose = require('mongoose');
 const Travel = require('../models/travel.models')
 
 module.exports.home = (req, res, next) => {
-    Travel.find({ $expr: { $lt: [{ $size: "$users" }, 15]} })
-        .then((travels) => res.render('home', { travels }))
+    Travel.find({ $expr: { $lt: [{ $size: "$users" }, 15] } })
+        .then((travels) => {
+            const formattedTravels = travels.map((travel) => ({
+                _id: travel._id,
+                date: travel.date.toLocaleString('es-ES', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                }),
+                destination: travel.destination
+            }));
+            res.render('home', { travels: formattedTravels });
+        })
         .catch((error) => next(error))
 }
